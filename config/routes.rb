35394a65bbf6af9c298d3a1f0bf7df4c1ac2beba
Jake Burton <jburto94@yahoo.com>
2022-08-04 18:33:51 -0400

    added user model and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  get "sign_up", to: "registrations#create"
+
   root to: "main#index"
 end
