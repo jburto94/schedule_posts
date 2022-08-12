703b3f36f3c905dc531cb4d5aed99548f5e62317
Jake Burton <jburto94@yahoo.com>
2022-08-11 20:34:21 -0400

    added twitter accounts page

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
 
-  get "auth/twitter/callback", to: "omniauth_callback#twitter"
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
+  resources :twitter_accounts
 
   root to: "main#index"
 end
